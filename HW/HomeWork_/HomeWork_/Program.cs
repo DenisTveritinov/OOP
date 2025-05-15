572b9284ef8e150a9e9205b38d9546450563aca6
Denis Tveritinof <deneek202@gmail.com>
2025-05-15 22:22:33 +0300

    pullReq

--- HW/HomeWork_/HomeWork_/Program.cs
+++ HW/HomeWork_/HomeWork_/Program.cs
@@ -6,7 +6,7 @@ class Program
     static void Main()
     {
         List<string> list = new List<string>();
-        list.Add("lololo");
+        list.Add("loloo");
         list.Add("lolo323o");
         list.Add("fdgfdgfd");
         TextContainer textContainer = new TextContainer(list);
