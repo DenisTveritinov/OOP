8d0320fc15d38186cc25ff9980f5d455d6c80462
Denis Tveritinof <deneek202@gmail.com>
2025-05-15 21:57:40 +0300

    finished_hw

---
+++ HW/HomeWork_/HomeWork_/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HomeWork_;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> list = new List<string>();
+        list.Add("lololo");
+        list.Add("lolo323o");
+        list.Add("fdgfdgfd");
+        TextContainer textContainer = new TextContainer(list);
+
+        Console.WriteLine(textContainer.getNumberVowels());
+        Console.WriteLine(textContainer[2]);
+    }
+}
---
+++ HW/HomeWork_/HomeWork_/TextContainer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_
+{
+    internal class TextContainer
+    {
+        private List<string> text;
+        public TextContainer(List<string> lines)
+        {
+            text = lines;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= text.Count)
+                    throw new IndexOutOfRangeException("Невірний індекс");
+                return text[index].Length;
+            }
+        }
+
+        public int getNumberVowels()
+        {
+            int count = 0;
+            char[] vowels = { 'a', 'i', 'e', 'o', 'u' };
+
+            foreach (string line in text)
+            {
+                foreach (char ch in line)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (ch == vowels[i])
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return count;
+
+        }
+    }
+}
